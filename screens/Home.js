27980dc9f420bd55e137adb917483ff4c137eea3
Ernest Anderson <hutasoit.ernest.anderson@gmail.com>
2022-05-10 21:34:07 +0700

    Slider Box styling for upcoming movies banner

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet, useWindowDimensions } from 'react-native';
 import { getPopularMovies, getUpcomingMovies } from '../services/services';
 import { SliderBox } from "react-native-image-slider-box";
 
 const Home = () => {
-    const [upcomingMoviesImages, setUpcomingMoviesImages] = useState(''); //destructirung array
+    const [upcomingMoviesImages, setUpcomingMoviesImages] = useState([]); //destructirung array
     const [error, setError] = useState(false); //destructuring array
   
     /*
@@ -18,26 +18,35 @@ const Home = () => {
           movies.forEach(movie => {
             upcomingMoviesImagesArray.push('https://image.tmdb.org/t/p/w500' + movie.poster_path);
           })
-          setUpcomingMoviesImages(upcomingMoviesImagesArray)
+          setUpcomingMoviesImages(upcomingMoviesImagesArray);
         })
         .catch(err => {
           setError(err);
         });
-        
-      getPopularMovies()
-        .then(movies => {
-          setMovie(movies[0])
-        })
-        .catch(err => {
-          setError(err);
-      });
     }, []);
     return (
-        <View>
-            <SliderBox images={upcomingMoviesImages} />
+        <View style={styles.sliderBoxContainer}>
+            <SliderBox 
+              images={upcomingMoviesImages} 
+              autoplay={true} 
+              circleLoop={true} 
+              sliderBoxHeight={useWindowDimensions().height / 1} 
+              dotStyle={styles.sliderBoxStyles}
+            />
             {error && <Text style={{color: 'red'}}>Error In The Server</Text>}
         </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+  sliderBoxContainer:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  sliderBoxStyles:{
+    height: 0
+  }
+});
 
 export default Home;
